1aba5d47a12af6e18fbfdbcd059afccfea6f01b4
pacoakm <pacoakm@gmail.com>
2022-10-07 12:27:27 +0800

    add things

--- 7-10/test.py
+++
@@ -1,4 +0,0 @@
-import math as mental_abuse_to_human
-y = mental_abuse_to_human.sqrt(16)
-print(y)
-
---
+++ ICT Lessons/7-10/test.py
@@ -0,0 +1,8 @@
+from math import sin
+
+
+a = 3
+b = 4
+c = 2
+area = 1 / 2 * a * b * sin(c)
+print(area)
