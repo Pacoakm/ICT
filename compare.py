514dcfdac19dae3b114d46dbcef8a9f8661a6e2c
pacoakm <pacoakm@gmail.com>
2023-02-12 17:31:03 +0800

    asdf

---
+++ compare.py
@@ -0,0 +1,44 @@
+import difflib
+import tkinter as tk
+from tkinter import filedialog
+
+# Open file dialog to select the first file
+root = tk.Tk()
+root.withdraw()
+print("Select the earlier .txt file")
+file1_path = filedialog.askopenfilename(
+    title="Select first file", filetypes=(("Text files", "*.txt"), ("All files", "*.*"))
+)
+print("Select the newest .txt file")
+# Open file dialog to select the second file
+file2_path = filedialog.askopenfilename(
+    title="Select second file",
+    filetypes=(("Text files", "*.txt"), ("All files", "*.*")),
+)
+
+# Read the contents of both files
+with open(file1_path, "r") as file1:
+    file1_contents = file1.readlines()
+
+with open(file2_path, "r") as file2:
+    file2_contents = file2.readlines()
+
+# Compare the contents of the two files using difflib
+differ = difflib.Differ()
+diff = list(differ.compare(file1_contents, file2_contents))
+fol = list("\n".join(diff).split("\n"))
+
+fol = [fol[i] for i in range(len(fol)) if i % 2 == 0]
+print(fol)
+if not diff:
+    print("No difference")
+else:
+    print("Unfollowers: ")
+    for i in fol:
+        if i[0] == "-":
+            print(i[2:])
+    print()
+    print("New followers:")
+    for i in fol:
+        if i[0] == "+":
+            print(i[2:])
--- followers2txt.py
+++ followers2txt.py
@@ -1,4 +1,3 @@
-
 import datetime
 import os
 from bs4 import BeautifulSoup
@@ -8,6 +7,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from requests.sessions import should_bypass_proxies
 from selenium.webdriver.common.action_chains import ActionChains
+
 # import required modules
 from bs4 import BeautifulSoup
 import requests
@@ -18,11 +18,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from time import sleep
+
 # create a new Chrome driver
 print("Starting...")
 driver = webdriver.Chrome()
-input_name = str(input("Username: "))
-input_pass = str(input("Password: "))
+input_name = str(input("Please enter your IG username: "))
+input_pass = str(input("Please enter your password: "))
 
 
 driver.get("https://www.instagram.com")
@@ -38,13 +39,19 @@ press_login = driver.find_element(By.XPATH, "//button[@type='submit']")
 press_login.click()
 sleep(15)
 try:
-    later = driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div/div/div/button")
+    later = driver.find_element(
+        By.XPATH,
+        "/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div/div/div/button",
+    )
     later.click()
 except:
     pass
 sleep(6)
 try:
-    later_noti = driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]")
+    later_noti = driver.find_element(
+        By.XPATH,
+        "/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]",
+    )
     later_noti.click()
 except:
     pass
@@ -59,17 +66,19 @@ input("Please scoll to the bottom. Press any key to continue...")
 
 
 req = driver.page_source
-soup = BeautifulSoup(req, 'html.parser')
-divs = soup.select('div._ab8y._ab94._ab97')
+soup = BeautifulSoup(req, "html.parser")
+divs = soup.select("div._ab8y._ab94._ab97")
 for i in divs:
     fol.append(i.text)
 fol.sort()
 now = datetime.datetime.now()
-filename = 'followers_{}.txt'.format(now.strftime('%Y-%m-%d_%H-%M-%S'))
+filename = "followers_{}.txt".format(now.strftime("%Y-%m-%d_%H-%M-%S"))
 
-with open(filename, 'w') as file:
+with open(filename, "w") as file:
     # Write each item in the list to a new line in the file
     for item in fol:
-        file.write(str(item) + '\n')
+        file.write(str(item) + "\n")
 
-print(f"A .txt file of followers at {format(now.strftime('%Y-%m-%d_%H-%M-%S'))} has been saved")
+print(
+    f"A .txt file of followers at {format(now.strftime('%Y-%m-%d_%H-%M-%S'))} has been saved"
+)
