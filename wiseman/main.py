fb6e61f105ef2d6774d52a67a2e340eaa99f4e4e
pacoakm <pacoakm@gmail.com>
2023-02-20 21:04:19 +0800

    add

--- wiseman/main.py
+++ wiseman/main.py
@@ -2,6 +2,7 @@ import datetime
 import os
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.alert import Alert
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
@@ -11,11 +12,8 @@ from selenium.webdriver.chrome.options import Options
 
 from bs4 import BeautifulSoup
 import requests
-
-
 import numpy as np
 import matplotlib.pyplot as plt
-
 from time import sleep
 
 # create a new Chrome driver
@@ -24,9 +22,17 @@ print("Starting...")
 
 options = Options()
 options.headless = False
-options.add_argument("--disable-gpu")
 options.add_argument("--disable-extensions")
 driver = webdriver.Chrome(options=options)
+
+
+def skip_alert():
+    try:
+        alert = Alert(driver)
+        alert_text = alert.text
+        alert.accept()
+    except:
+        pass
 
 
 def login():
@@ -37,8 +43,8 @@ def login():
         upload_throughput=500 * 1024,
     )
     driver.get("https://lms.wiseman.com.hk/lms/user/")
-    input_name = "sy.sy10363"
-    input_pass = "Akm20070722"
+    input_name = "sy.sy10395"
+    input_pass = "2022"
     print("Logging in. Please wait...")
     while True:
         try:
@@ -80,17 +86,21 @@ next_button = 0
 input_lesson = input("Input the lesson link...")
 ans = []
 driver.get(input_lesson)
+sleep(3)
 for i in range(20):
-    if i == 3:
+    if i == 2:
         driver.set_network_conditions(
             offline=True, latency=100000, download_throughput=0, upload_throughput=0
         )
     sleep(0.5)
+    skip_alert()
+
     if i == 0:
         frame = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, "iframe#course"))
         )
         driver.switch_to.frame(frame)
+    skip_alert()
 
     while True:
         try:
@@ -107,7 +117,7 @@ for i in range(20):
     divs = soup.select("span.fix-answer.ng-star-inserted")
     for i in divs:
         ans.append(i.text)
-
+    skip_alert()
     try:
         next_button = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located(
@@ -118,12 +128,23 @@ for i in range(20):
     except:
         break
     sleep(0.5)
+    skip_alert()
 
 print(ans)
-driver.quit()
+
+driver.execute_script("window.open('');")
+driver.delete_all_cookies()
+driver.close()
+
+driver.switch_to.window(driver.window_handles[0])
+
+driver.set_network_conditions(
+    offline=False,
+    latency=10,
+    download_throughput=500 * 1024,
+    upload_throughput=500 * 1024,
+)
 login()
-
-
 driver.get(input_lesson)
 
 for i in range(20):
@@ -135,9 +156,11 @@ for i in range(20):
         driver.switch_to.frame(frame)
 
     while True:
+        driver.switch_to.frame(frame)
         try:
             input_ans = driver.find_element(
-                By.CLASS_NAME, "ng-pristine.ng-valid.ng-star-inserted.ng-touched"
+                By.CSS_SELECTOR,
+                'input[autocomplete="off"][class="ng-untouched ng-pristine ng-valid ng-star-inserted"]',
             )
             input_ans.send_keys(ans[i])
             break
